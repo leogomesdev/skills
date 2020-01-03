1d9bd4e63a2e13d2fd6671dd46cf4fa4ab9c84ee
Leonardo Gomes <leogomesdev@gmail.com>
2020-01-03 05:54:29 -0300

    #003 add more tests

---
+++ src/tests/factories.js
@@ -0,0 +1,34 @@
+export function invalidSkill() {
+  return {
+    name: 'invalidSkill',
+    level: 'AAAA'
+  }
+}
+
+export function juniorSkill() {
+  return {
+    name: 'juniorSkill',
+    level: 'JUNIOR'
+  }
+}
+
+export function seniorSkill() {
+  return {
+    name: 'seniorSkill',
+    level: 'SENIOR'
+  }
+}
+
+export function seniorSkillLowercase() {
+  return {
+    name: 'seniorSkill',
+    level: 'senior'
+  }
+}
+
+export function expertSkill() {
+  return {
+    name: 'expertSkill',
+    level: 'EXPERT'
+  }
+}
--- src/tests/repositories/skill.test.js
+++ src/tests/repositories/skill.test.js
@@ -1,14 +1,10 @@
 import assert from 'assert';
 import * as skillRepository from '../../repositories/skill-repository';
-import Skill from '../../models/skill';
 const dbHandler = require('../dbHandler');
+import * as factory from '../factories';
+import { expect } from 'chai';
 
-const skillOne = {
-  name: 'Skill1',
-  level: 'SENIOR'
-};
 const oneHour = 60 * 60 * 1000;
-
 
 describe('Skill Create operations', () => {
   before(async () => {
@@ -19,7 +15,7 @@ describe('Skill Create operations', () => {
   });
 
   it('Creates a skill successfully', (done) => {
-    const skill = skillRepository.createSkill(skillOne)
+    const skill = skillRepository.createSkill(factory.juniorSkill())
       .then((skill) => {
         assert(!skill.isNew);
         done();
@@ -27,7 +23,7 @@ describe('Skill Create operations', () => {
   });
 
   it('Creates a skill must fill the field createdAt', (done) => {
-    const skill = skillRepository.createSkill(skillOne)
+    const skill = skillRepository.createSkill(factory.juniorSkill())
       .then((skill) => {
         assert(skill.createdAt);
         done();
@@ -35,7 +31,7 @@ describe('Skill Create operations', () => {
   });
 
   it('Creates a skill must fill the field createdAt with a recent (current) Datetime', (done) => {
-    const skill = skillRepository.createSkill(skillOne)
+    const skill = skillRepository.createSkill(factory.juniorSkill())
       .then((skill) => {
         assert(((Date.now()) - new Date(skill.createdAt)) < oneHour);
         done();
@@ -43,7 +39,7 @@ describe('Skill Create operations', () => {
   });
 
   it('Creates a skill must allow `junior` level', (done) => {
-    const skill = skillRepository.createSkill({ name: 'skill2', level: 'junior' })
+    const skill = skillRepository.createSkill(factory.juniorSkill())
       .then((skill) => {
         assert(!skill.isNew);
         done();
@@ -51,7 +47,7 @@ describe('Skill Create operations', () => {
   });
 
   it('Creates a skill must allow `senior` level', (done) => {
-    const skill = skillRepository.createSkill({ name: 'skill2', level: 'senior' })
+    const skill = skillRepository.createSkill(factory.seniorSkill())
       .then((skill) => {
         assert(!skill.isNew);
         done();
@@ -59,7 +55,7 @@ describe('Skill Create operations', () => {
   });
 
   it('Creates a skill must allow `expert` level', (done) => {
-    const skill = skillRepository.createSkill({ name: 'skill2', level: 'expert' })
+    const skill = skillRepository.createSkill(factory.expertSkill())
       .then((skill) => {
         assert(!skill.isNew);
         done();
@@ -67,9 +63,9 @@ describe('Skill Create operations', () => {
   });
 
   it('Creates a skill must convert level to uppercase', (done) => {
-    const skill = skillRepository.createSkill({ name: 'skill2', level: 'senior' })
+    const skill = skillRepository.createSkill(factory.seniorSkillLowercase())
       .then((skill) => {
-        Skill.findOne({ name: 'skill2' })
+        skillRepository.getSkill(skill.id)
           .then((skillFounded) => {
             assert(skillFounded.level === 'SENIOR');
             done();
@@ -78,10 +74,119 @@ describe('Skill Create operations', () => {
   });
 
   it('Creates a skill must not allow level AAAA', (done) => {
-    const skill = skillRepository.createSkill({ name: 'skill2', level: 'AAAA' })
+    const skill = skillRepository.createSkill(factory.invalidSkill())
       .catch((err) => {
         assert(err.errors.level.message === '`AAAA` is not a valid enum value for path `level`.')
         done();
+      });
+  });
+
+  after(async () => {
+    await dbHandler.closeDatabase();
+  });
+
+});
+
+describe('Skill Read operations', () => {
+  before(async () => {
+    await dbHandler.connect();
+  });
+  afterEach(async () => {
+    await dbHandler.clearDatabase();
+  });
+
+  it('Get a Skill by id', (done) => {
+    const seniorSkill = skillRepository.createSkill(factory.seniorSkill())
+      .then((skill) => {
+        skillRepository.getSkill(skill.id)
+          .then((skillFounded) => {
+            assert(skillFounded.name === 'seniorSkill');
+            done();
+          });
+      });
+  });
+
+  it('List all skills', (done) => {
+    const juniorSkill = skillRepository.createSkill(factory.juniorSkill());
+    const seniorSkill = skillRepository.createSkill(factory.seniorSkill())
+      .then(() => {
+        skillRepository.listSkills()
+          .then((skillsFounded) => {
+            assert(skillsFounded.length == 2);
+            done();
+          });
+      });
+  });
+
+  after(async () => {
+    await dbHandler.closeDatabase();
+  });
+});
+
+describe('Skill Update operations', () => {
+  before(async () => {
+    await dbHandler.connect();
+  });
+  afterEach(async () => {
+    await dbHandler.clearDatabase();
+  });
+
+  it('Updates a Skill must allow to update the name', (done) => {
+    const skill = skillRepository.createSkill(factory.seniorSkill())
+      .then((skill) => {
+        skillRepository.updateSkill(skill.id, { name: 'newName' })
+          .then(() => skillRepository.getSkill(skill.id)
+            .then((skillFounded) => {
+              assert(skillFounded.name === 'newName');
+              done();
+            }));
+      });
+  });
+
+  it('Updates a Skill must allow to update the level to another valid level', (done) => {
+    const skill = skillRepository.createSkill(factory.seniorSkill())
+      .then((skill) => {
+        skillRepository.updateSkill(skill.id, { level: 'EXPERT' })
+          .then(() => skillRepository.getSkill(skill.id)
+            .then((skillFounded) => {
+              assert(skillFounded.level === 'EXPERT');
+              done();
+            }));
+      });
+  });
+
+  it('Updates a Skill must allow to update name and level', (done) => {
+    const skill = skillRepository.createSkill(factory.seniorSkill())
+      .then((skill) => {
+        skillRepository.updateSkill(skill.id, { name: 'updated name', level: 'EXPERT' })
+          .then(() => skillRepository.getSkill(skill.id)
+            .then((skillFounded) => {
+              assert(skillFounded.name === 'updated name');
+              assert(skillFounded.level === 'EXPERT');
+              done();
+            }));
+      });
+  });
+
+  it('Updates a Skill must not allow invalid level', (done) => {
+    const skill = skillRepository.createSkill(factory.seniorSkill())
+      .then((skill) => {
+        skillRepository.updateSkill(skill.id, { level: 'INVALID' })
+          .catch((err) => {
+            assert(err.errors.level.message === '`INVALID` is not a valid enum value for path `level`.')
+            done();
+          });
+      });
+  });
+
+  it('Updates a Skill must not allow empty name', (done) => {
+    const skill = skillRepository.createSkill(factory.seniorSkill())
+      .then((skill) => {
+        skillRepository.updateSkill(skill.id, { name: '' })
+          .catch((err) => {
+            assert(err.errors.name.message === 'Path `name` is required.')
+            done();
+          });
       });
   });
 
@@ -100,15 +205,15 @@ describe('Skill Delete operations', () => {
   });
 
   it('Deletes a Skill', (done) => {
-    const skill = skillRepository.createSkill({ name: 'skill2', level: 'senior' })
+    const skill = skillRepository.createSkill(factory.seniorSkill())
       .then((skill) => {
         skillRepository.deleteSkill(skill.id)
-          .then(() => Skill.findOne({ name: 'Skill1' }))
-          .then((skillFounded) => {
-            assert(skillFounded === null);
-            done();
-          });
-      })
+          .then(() => skillRepository.getSkill(skill.id)
+            .then((skillFounded) => {
+              assert(skillFounded === null);
+              done();
+            }));
+      });
   });
 
   after(async () => {
